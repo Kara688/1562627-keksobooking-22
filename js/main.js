ae6489fda1324f8fd0a944273ce2719752b3c83f
Kara688 <70812371+Kara688@users.noreply.github.com>
2021-01-31 20:12:28 +0300

    function added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
+
+  alert( randomInteger(1, 3) );
+
+ // (https://learn.javascript.ru/task/random-int-min-max)
