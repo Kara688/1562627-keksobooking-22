db8e757562a896550fd4653ba7056e8a0493d7bb
Kara688 <70812371+Kara688@users.noreply.github.com>
2021-02-07 22:07:54 +0300

    Objects

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ function getRandomFloat( from, to, decimal = 2) {
     return + (Math.random() * (to - from) + from).toFixed (decimal)
   }
 }
+
+
+const types = ['flat', 'bungalow', 'palace']
+const MIN_PRICE = 1
+const MAX_PRICE = 10000
+
+const getRandomOffer = () => {
+  return {
+    offer: {
+     type:
+     types[Math.floor(Math.random()*types.length)];
+
+     price:
+
+     if (MIN_PRICE > MAX_PRICE && MAX_PRICE >= 0) {
+      return + (Math.random() * (MIN_PRICE - MAX_PRICE) + MAX_PRICE).toFixed(3)
+    }
+    }
+
+    }
+  }
+}
