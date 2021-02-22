bb1abb1db3ba5a600f1f6a4eb5c1be7d0a9eb23c
Kara688 <70812371+Kara688@users.noreply.github.com>
2021-02-22 12:06:01 +0300

    Objects

--- js/main.js
+++ js/main.js
@@ -4,10 +4,7 @@ function getRandomInteger (min, max) {
   let rand = min - 0.5 + Math.random () * (max - min + 1);
   return Math.round (rand);
 }
-
-console.log(getRandomInteger (1, 3) );
-
- // (https://learn.javascript.ru/task/random-int-min-max)
+// (https://learn.javascript.ru/task/random-int-min-max)
 
 function getRandomFloat( from, to, decimal = 2) {
   if (to > from && from >= 0) {
@@ -21,19 +18,19 @@ function getRandomElement(items) {
 
 const types = ['palace', 'flat', 'house', 'bungalow']
 const checks = ['12:00', '13:00', '14:00']
-const titles = ['двухместный номер', 'трёхместный номер', 'номер кинг сайз']
+const title = ['двухместный номер', 'трёхместный номер', 'номер кинг сайз']
 const rooms =  [25, 30, 45 ]
 const guests = [2, 1]
 
 
 
 
-function getCreateOffer() {
+function CreateOffer() {
   let array = []
   for(let i = 0; i <= 10; i++) {
     const offer = {
       author: {
-        avatar:'img/avatars/user01.png'
+        avatar: 'img/avatars/user01.png',
       },
       offer: {
         title:  getRandomElement(title),
@@ -47,14 +44,14 @@ function getCreateOffer() {
         features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
         description: 'small',
         photos: [
-        'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-        'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-        'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+          'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+          'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+          'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
       },
       location: {
         x: getRandomFloat (35.65000, 35.70000),
-        y: getRandomFloat(139.70000, 139.80000)
-      }
+        y: getRandomFloat(139.70000, 139.80000),
+      },
 
     }
 
@@ -67,7 +64,7 @@ function getCreateOffer() {
 
 }
 
- let myFunc = getCreateOffer()
- console.log(myFunc)
+let offers = CreateOffer();
+
 
 
