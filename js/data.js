dae363d433b0d6ec874b751460e7e87fe1adaf25
Kara688 <70812371+Kara688@users.noreply.github.com>
2021-02-24 01:05:50 +0300

    Modules

---
+++ js/data.js
@@ -0,0 +1,11 @@
+
+
+const types = ['palace', 'flat', 'house', 'bungalow']
+const checks = ['12:00', '13:00', '14:00']
+const titles = ['двухместный номер', 'трёхместный номер', 'номер кинг сайз']
+const rooms =  [25, 30, 45 ]
+const guests = [2, 1]
+
+export(types, checks, titles, rooms, guests)
+
+
