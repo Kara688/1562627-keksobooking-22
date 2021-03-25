d4c94b0d152b48fee4b04145311254021808c3f1
Kara688 <70812371+Kara688@users.noreply.github.com>
2021-03-26 00:56:52 +0300

    popup

--- js/card.js
+++ js/card.js
@@ -1,10 +1,16 @@
-const renderAd = (adData) => {
-  const offers = offerTemplate.cloneNode(true);
-  ad.querySelecor('.popup__title').textConent = adData.offer.title;
-  ad.querySelecor('.popup__text-address').textConent = adData.offer.address;
-  ad.querySelecor('.popup__text--price').innerHTML = adData.offer.price;
-  ad.querySelecor('.popup__type').textConent = adData.offer.type
+function renderIt() {
+ let offersContent = document.querySelector('#card');
+ let clone = offersContent.textContent.clodeNode(true);
 
-  return renderAd
+  h3.querySelector("popup_title").textContent = offer.offer.tittle;
+  p.querySelector("").textContent = offer.offer.price;
+  h4.querySelector("popup__type").textContent = offer.offer.type;
+  li.querySelector("popup__feature popup__feature--wifi").textContent = offer.offer.feature.wifi;
+  li.querySelector("popup__feature popup__feature--dishwasher").textContent = offer.offer.feature.dishwasher;
+  li.querySelector("popup__feature popup__feature--parking").textContent = offer.offer.feature.parking;
+  li.querySelector("popup__feature popup__feature--washer").textContent = offer.offer.feature.washer;
+  li.querySelector("popup__feature popup__feature--elevator").textContent = offer.offer.feature.elevator;
+  li.querySelector("popup__feature popup__feature--conditioner").textContent = offer.offer.feature.conditioner;
+
+    return (renderIt);
 }
-
